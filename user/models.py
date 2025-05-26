337649211bb08130d338e487b092e1e43eb7b101
Maximus Champion <maximuschampion2002@gmail.com>
2025-05-26 13:33:54 +0300

    Stop tracking .env file

---
+++ user/models.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext as _
+
+from django.db import models
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(_("email address"), unique=True)
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
